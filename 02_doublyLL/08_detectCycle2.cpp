d2c9b35212bff4cdbec8a4212347aeacbeebbc5e
Singh_Rajanshu <112928611+RajanshuSingh@users.noreply.github.com>
2025-03-28 22:14:05 +0530

    detecting the element from where the loop began using tortoise n hare.

---
+++ 02_doublyLL/08_detectCycle2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath> 
+using namespace std;
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node()
+            {
+                this->data = 0;
+                next = NULL;
+            }
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+            Node(int data, Node* next)
+            {
+                this->data = data;
+                this->next = next;
+            }
+        };
+
+Node *firstNode(Node *head)
+{
+    
+
+    Node* temp=head;
+    Node* fast=head;
+    Node* slow=head;
+
+    while(fast!=NULL && fast->next!= NULL ){
+
+        fast=fast->next->next;
+        slow=slow->next;
+
+        if(slow==fast){
+            slow=head;
+            while(fast!=slow){
+                 slow=slow->next;
+            fast=fast->next;
+            }
+
+            return fast;
+            
+            
+        }
+
+
+
+    }
+
+    return NULL;
+}
